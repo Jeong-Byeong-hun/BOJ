1f890dde3427e94dba0c2d967b7e919b61a76924
qwerty95 <j95hun@naver.com>
2021-07-06 14:27:51 +0900

    7/6

---
+++ Python/BOJ1292.py
@@ -0,0 +1,6 @@
+#쉽게 푸는 문제
+
+n, m = map(int, input().split(' '))
+
+temp = []
+
---
+++ Python/ProCamouflage.py
@@ -0,0 +1,12 @@
+def solution(clothes):
+    answer = 0
+
+
+
+
+
+    return answer
+
+
+testCase = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+solution(testCase)
