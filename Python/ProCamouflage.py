b218b27861e837f70030c575f46a1c5dcd762265
qwerty95 <j95hun@naver.com>
2021-07-06 15:58:18 +0900

    7 / 6

--- Python/ProCamouflage.py
+++ Python/ProCamouflage.py
@@ -1,12 +1,46 @@
 def solution(clothes):
-    answer = 0
+    test_dict = {}
+    for i in clothes:
+        if i[1] in test_dict:
+            test_dict[i[1]] += 1
+        else:
+            test_dict[i[1]] = 1
+
+    cnt = 1
+    for i in test_dict.values():
+        cnt = cnt * (i + 1)
+
+    return cnt - 1
 
 
-
-
-
-    return answer
-
-
-testCase = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+testCase = [["a", "aa"],
+            ["b", "aa"],
+            ["c", "aa"],
+            ["aa", "bb"],
+            ["bb", "bb"],
+            ["c_c", "bb"],
+            ["aaa", "cc"],
+            ["bbb", "cc"],
+            ["ccc", "cc"]]
 solution(testCase)
+
+# testcase
+# // 63
+# [
+# ["a","aa"],
+# ["b","aa"],
+# ["c","aa"],
+# ["aa","bb"],
+# ["bb","bb"],
+# ["c_c","bb"],
+# ["aaa","cc"],
+# ["bbb","cc"],
+# ["ccc","cc"]
+# ]
+#
+# // 7
+# [
+# ["a", "a"],
+# ["b", "b"],
+# ["c", "c"]
+# ]
